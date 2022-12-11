95967a58d6ec50919bd71fbc317e7bed74356c7a
likith sai <likithsai13@gmail.com>
2022-12-11 22:53:53 +0530

    added -y flag for overiding files

--- ffmpeg-custom.js
+++ ffmpeg-custom.js
@@ -4,11 +4,11 @@ const utils = require('./src/utils/utils');
 const convertVideos = (fileList) => {
     fileList.forEach(element => {
         let newfileName = element.basepath + '/new_' + element.name;
-        let args = `-i ${element.path} -c:v libx265 -crf 27 -preset veryfast -vtag hvc1 -c:a copy ${newfileName}`;
+        let args = `-y -i ${element.path} -c:v libx265 -crf 27 -preset veryfast -vtag hvc1 -c:a copy ${newfileName}`;
         utils.executeCMD('ffmpeg', args.split(' '), data => {
             console.log(data);
         }, () => {
-            console.log('\x1b[36m%s\x1b[0m', `finished converting ${element.name}`);
+            console.log('\x1b[36m%s\x1b[0m', `finished optimizing ${element.name} video file`);
         });
     });
 }
@@ -25,14 +25,14 @@ if (process.argv[2]) {
             path: 'public/1.mp4',
             size: '7.32 MB',
             basepath: 'public/'
-        },
-        {
-            id: 58244300,
-            name: 'video.mp4',
-            path: 'public/video.mp4',
-            size: '13.28 MB',
-            basepath: 'public/'
         }
+        // {
+        //     id: 58244300,
+        //     name: 'video.mp4',
+        //     path: 'public/video.mp4',
+        //     size: '13.28 MB',
+        //     basepath: 'public/'
+        // }
     ]);
 } else {
     console.log('Please enter folder path!');
