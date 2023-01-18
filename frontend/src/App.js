ad1e76d75cc9c278e24dc4491d02bdbe9ebb5863
likith sai <likithsai13@gmail.com>
2023-01-18 11:29:38 +0530

    created header comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 import './scss/App.scss';
 
 const App = () => {
     return (
-        <h1 className="tw-text-3xl tw-font-bold tw-underline">
-            Hello world!
-        </h1>
+        <>
+            <Header />
+        </>
     )
 }
 
