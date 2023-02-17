07feeea312f51ea868f48fd404eaf0ede3f6fb74
likith sai <likithsai13@gmail.com>
2023-02-17 10:58:54 +0530

    added footer comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './scss/App.scss';
 
 const App = () => {
     return (
         <>
             <Header />
+            <Footer />
         </>
     )
 }
