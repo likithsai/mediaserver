17a975a1c50775f8bd2ab3fd6fb84faad3903d60
likith sai <likithsai13@gmail.com>
2023-03-03 07:56:00 +0530

    install node-sass and tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import './assets/scss/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <p>sample example</p>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
     </div>
   );
 }
